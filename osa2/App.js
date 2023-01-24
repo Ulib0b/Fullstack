82b26084efbb4c789203cc27705556be1d372a11
Ulib0b <91346667+Ulib0b@users.noreply.github.com>
2023-01-24 12:35:46 +0200

    2.5

--- osa2/App.js
+++ osa2/App.js
@@ -1,56 +1,54 @@
-const Course = (props) => {
-  const { course } = props
-
-  const parts = course.parts
-
-  const total = parts.reduce(
-    (i,value) => i+value.exercises
-  ,0)
-
-  return(
-    <>
-      <h1>{course.name}</h1>
-      <ul>
-        {parts.map(part => 
-          <li key={part.id}>
-            {part.name} {part.exercises}
-          </li>
-        )}
-
-        <li>Total amount of exercises {total}</li>
-      </ul>
-    </>
-  )
-
-  
-}
+import Course from "./Course"
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    id: 1,
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      }
-    ]
-  }
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
 
   return (
     <div>
-      <Course course={course} />
+      <Course courses={courses} />
     </div>
   )
 }
