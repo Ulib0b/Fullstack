86ce0081d2133bdbfea5ddf9630af7c680f2dc00
Ulib0b <91346667+Ulib0b@users.noreply.github.com>
2023-01-26 13:30:50 +0200

    2.7

--- osa2/puhelinluettelo/App.js
+++ osa2/puhelinluettelo/App.js
@@ -2,21 +2,30 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas',
-      id: 1 
+    {
+      name: 'Arto Hellas',
+      id: 1
     }
-  ]) 
+  ])
   const [newName, setNewName] = useState('')
 
   const addNimi = (event) => {
     event.preventDefault()
 
-    const obj = {
-      name: newName,
-      id: persons.length + 1
+    const sama = persons.filter(person => person.name === newName)
+
+    if (sama.length === 0) {
+      const obj = {
+        name: newName,
+        id: persons.length + 1
+      }
+
+      setPersons(persons.concat(obj))
+    }else{
+      alert(`${newName} on jo lisätty`)
     }
 
-    setPersons(persons.concat(obj))
+
     setNewName("")
   }
 
@@ -29,7 +38,7 @@ const App = () => {
       <h2>Phonebook</h2>
       <form onSubmit={addNimi}>
         <div>
-          name: <input 
+          name: <input
             value={newName}
             onChange={handleNameChange}
           />
@@ -40,12 +49,12 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <ul>
-          {persons.map(name => 
-            <li key={name.id}>
-              {name.name}
-            </li>
-          )}
-        </ul>
+        {persons.map(name =>
+          <li key={name.id}>
+            {name.name}
+          </li>
+        )}
+      </ul>
     </div>
   )
 
