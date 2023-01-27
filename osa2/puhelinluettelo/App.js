928d84ea11af9687636cc7d308364a09acd9d63c
Ulib0b <91346667+Ulib0b@users.noreply.github.com>
2023-01-27 13:54:41 +0200

    2.10

--- osa2/puhelinluettelo/App.js
+++ osa2/puhelinluettelo/App.js
@@ -1,5 +1,52 @@
 import { useState } from 'react'
 
+const Persons = (props) => {
+
+  const { persons } = props
+  return (
+    <ul>
+      {persons.map(name =>
+        <li key={name.id}>
+          {name.name} {name.number}
+        </li>
+      )}
+    </ul>
+  )
+}
+
+const Form = (props) => {
+
+  const newName = props.newName
+  const newNumber = props.newNumber
+  const addNimi = props.addNimi
+  const handleNameChange = props.handleNameChange
+  const handleNumberChange = props.handleNumberChange
+
+  
+  return(
+    <form onSubmit={addNimi}>
+      <div>
+        name: <input
+          value={newName}
+          onChange={handleNameChange}
+        />
+
+      </div>
+      <div>
+        number: <input
+          type="number"
+          value={newNumber}
+          onChange={handleNumberChange}
+        />
+      </div>
+      <div>
+        <button type="submit">add</button>
+      </div>
+    </form>
+  )
+  
+}
+
 const App = () => {
   const [persons, setPersons] = useState([
     {
@@ -24,7 +71,7 @@ const App = () => {
       }
 
       setPersons(persons.concat(obj))
-    }else{
+    } else {
       alert(`${newName} on jo lisätty`)
     }
 
@@ -44,33 +91,16 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={addNimi}>
-        <div>
-          name: <input
-            value={newName}
-            onChange={handleNameChange}
-          />
-          
-        </div>
-        <div>
-          number: <input 
-              type="number"
-              value={newNumber}
-              onChange={handleNumberChange}
-          />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
+      <Form 
+      newName={newName} 
+      newNumber={newNumber} 
+      addNimi={addNimi} 
+      handleNameChange={handleNameChange}
+      handleNumberChange={handleNumberChange}
+      >
+      </Form>
       <h2>Numbers</h2>
-      <ul>
-        {persons.map(name =>
-          <li key={name.id}>
-            {name.name} {name.number}
-          </li>
-        )}
-      </ul>
+      <Persons persons={persons}></Persons>
     </div>
   )
 
