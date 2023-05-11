07f6be9ec901da6f40f6febc093a1298f2b06716
Ulib0b <91346667+Ulib0b@users.noreply.github.com>
2023-05-11 16:24:23 +0300

    Create index.js

---
+++ osa4/index.js
@@ -0,0 +1,30 @@
+require('dotenv').config()
+const http = require('http')
+const express = require('express')
+require('express-async-errors')
+const app = express()
+const cors = require('cors')
+const middleware = require('./utils/middleware')
+const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
+
+
+
+app.use(cors())
+app.use(express.json())
+app.use(middleware.requestLogger)
+
+app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+
+const PORT = 3003
+app.listen(PORT, () => {
+})
+
+module.exports = app
